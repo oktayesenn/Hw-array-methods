270c463202f36fd4984f4e64ab576786ea6b9cb7
Oktay esen <oktayesenn@gmail.com>
2022-05-05 09:38:53 -0400

    Oktay

--- js/script.js
+++ js/script.js
@@ -2,23 +2,55 @@ console.log("script file wired up and ready to roll!!!")
 
 // 1. allNames:
 // Use .forEach to print all persons names in the console.
+// data.forEach(allNames => console.log(`${allNames.name}`))
 
 // 2. totalAgeAllPersons:
 // Find the total combined age of all persons.
+// const totalAgeAllPersons = data.reduce((totalAge, ages) => {
+//         return totalAge + ages.age
+//     }, 0)
+    
+//     console.log(totalAgeAllPersons)
+
 
 // 3. totalAgeAllPets:
 // Find the total combined age of all pets.
 
+
+// const totalAgeAllPets = data.reduce((totalAgePets,agePets) => {
+//     for (i = 0; i < agePets.pets.length; i++)
+//         totalAgePets += agePets.pets[i].age
+//         return totalAgePets
+// }, 0)
+
+// console.log(totalAgeAllPets)
+
 // 4. oldEnough:
 // Use .filter() to create an array of people old enough to be president.
+
+// const filteredAges = data.filter(num => {
+//     return num.age > 50
+// })
+// console.log(filteredAges)
 
 // 5. sadPeople:
 // Use .filter() to create an array of "sad" people (people with no pets).
 
+// const filtered = data.filter(no => no.pets == "")
+// console.log(filtered)
+
+
 // 6. warAndPeace:
 // Use.map() to create an array called warAndPeace that has, in each index the string "war" or "peace"
 //   * the string "war" if the person at that has BOTH a cat AND a dog.
-//   * the string "peace" otherwise.
+//   * the string "peace" otherwise.0
+
+// function war(data) {
+//     let both = data.war(yes => yes.cat & yes.dog =="yes")
+//     return both
+//   }
+  
+//   const war = data.map(both)
 
 // 7. justSpock:
 // Create an array of all the pet objects where the pets named is "spock".
